49b283f7865244753330b988ab881b3ac81ca0c4
Andreas Sæbjørnsen <andreas@digitalplaywright.com>
2014-04-16 14:16:25 -0700

    fixes to template

--- app/controllers/posts/show.js
+++ app/controllers/posts/show.js
@@ -1,5 +1,10 @@
 var PostsShowController =  Ember.ObjectController.extend({
+	actions: {
+	    edit: function(post) {
+	      this.transitionToRoute('posts.edit',post);
 
+	    }
+	}
 });
 
 export default PostsShowController;
