9fb250ec25c9ac798dc4089f8bd8b3b4a658629e
Andreas Sæbjørnsen <andreas@digitalplaywright.com>
2014-04-16 14:07:00 -0700

    fixes

--- app/controllers/posts/edit.js
+++ app/controllers/posts/edit.js
@@ -1,4 +1,22 @@
 var PostsEditController =  Ember.ObjectController.extend({
+    actions: {
+	    save: function() {
+	      var _self = this;
+
+	      function redirectToShow() {
+	        _self.transitionToRoute('posts.show', _self.get('model'));
+	      }
+	      function handleErrors(reason) {
+	        // if reason instanceof DS.InvalidError
+	        //  model.errors will contain the server-supplied validation errors
+	        // else
+	        //  isError will be true and we'll show a generic "seems bad" message
+	      }
+
+
+	      this.get("model").save().then(redirectToShow).catch(handleErrors);
+	    }
+	}
 });
 
 export default PostsEditController;
