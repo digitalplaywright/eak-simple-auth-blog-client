ac0b53ba76ec282b3bdfe4d71c606fc9056e500f
Andreas Sæbjørnsen <andreas@digitalplaywright.com>
2014-04-23 15:37:59 -0700

    add declarative rules

---
+++ app/libs/declarative-rules.js
@@ -0,0 +1,133 @@
+
+var DeclarativeRules = Ember.Object.extend({
+	activities: [ 
+	    {
+	      activity: 'edit',
+
+	      can:  function(actor, object, target) {
+	      	return true;
+		  }
+		    
+	    }
+
+	],
+	activities_hash: {},
+
+    required_properties: ["activity","can"],
+	optional_properties: ["actor","object", "target"],
+	hash_key_separator: "-",
+
+    validateFormat: function(item, require_can_definition){
+    	this.required_properties.forEach(function(prop_name, index, enumerable){
+    		if ( require_can_definition == false && prop_name == "can" ){
+
+    		}else if( item[prop_name] == undefined )
+			{
+				var message = "Error: Missing required property ";
+				message = message.concat(JSON.stringify(prop_name) );
+				message = message.concat(" in: ", JSON.stringify(item) );
+
+				throw message;
+			}
+			
+	    });
+    },
+
+    get_hash_key: function(item, require_can_definition){
+    	var self = this;
+
+		var hash_key="";
+
+    	var expected_properties = this.required_properties.concat(this.optional_properties);
+
+        self.validateFormat(item, require_can_definition);
+
+		expected_properties.forEach(function(prop_name, index, enumerable){
+            var cur_prop = item[prop_name];
+
+			if(prop_name != "can" && cur_prop != undefined){ 
+			    if (index != 0)	{ hash_key = hash_key.concat(self.hash_key_separator) }		
+
+			    if (  typeof cur_prop === 'string' ){
+					hash_key = hash_key.concat(cur_prop);
+				}else{
+					var cur_type = 
+					hash_key = hash_key.concat(self.type_of(cur_prop));
+				}
+
+			}
+
+		});
+
+		return hash_key;
+    },
+
+    make_activities_hash: function(){
+      	var self = this;
+
+     	this.activities.forEach(function(item, index, enumerable){
+			var hash_key = self.get_hash_key(item, true);
+
+			if( self.activities_hash.hasOwnProperty(hash_key) ){
+				var message = "Error: Activity with the same definition already exists for ";
+				message = message.concat( JSON.stringify(item) );
+
+				throw message;
+			}
+
+	    	self.activities_hash[hash_key] = item.can;
+
+	    });
+
+    },
+
+    init: function(){
+        this.make_activities_hash();
+    },
+    
+	type_of: function(object){
+
+      if(DS.Model.detectInstance(object)){
+      	return object.constructor.typeKey;
+      }else if(DS.RecordArray.detectInstance(object)){
+      	return object.type.typeKey;
+      }else{
+
+		var message = "Error: Type unhandled for ";
+		message = message.concat(message, JSON.stringify(object) );
+
+		throw message;
+      }
+
+	},
+
+    can: function( params_hash ) {
+
+    	var hash_key = this.get_hash_key(params_hash, false);
+    	console.log('hash key is');
+    	console.log(hash_key);
+    	console.log(this.activities_hash.hasOwnProperty(hash_key));
+
+		if( !this.activities_hash.hasOwnProperty(hash_key) ){
+			var message = "Error: no matching activity definition found for ";
+			message = message.concat(JSON.stringify(params_hash) );
+
+			throw message;
+		}
+
+		return this.activities_hash[hash_key](params_hash["actor"],
+			params_hash["object"], params_hash["target"]);
+
+    },
+
+    cannot: function( params_hash ) {
+    	return !can(params_hash);
+    }
+
+
+
+});
+
+
+
+export default DeclarativeRules;
